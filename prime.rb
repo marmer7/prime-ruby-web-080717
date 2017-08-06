0d0679311b85467d9ea316a25cbca7ef844a77a0
Marlon Merjos <marlonmerjos@gmail.com>
2017-08-06 16:35:19 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(n)
+  if n == 0 || n < 0 || n == 1
+    return false
+  end
+
+  half = (n/2).floor
+
+  for x in (2..half)
+    if n % x == 0 || n == x
+      return false
+    end
+  end
+  return true
+end
